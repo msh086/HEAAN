6960029e5b3e9a3111402d0f49cbec625cda57dd
msh086 <1729092048@qq.com>
2021-03-17 17:49:02 +0800

    modify makefile to build HEAAN with debug info

--- HEAAN/lib/src/subdir.mk
+++ HEAAN/lib/src/subdir.mk
@@ -59,7 +59,7 @@ CPP_DEPS += \
 src/%.o: ../src/%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++ -I/usr/local/include -O3 -c -std=c++11 -pthread -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	g++ -I/usr/local/include -O3 -c -std=c++11 -pthread -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<" $(CXXFLAGS)
 	@echo 'Finished building: $<'
 	@echo ' '
 
